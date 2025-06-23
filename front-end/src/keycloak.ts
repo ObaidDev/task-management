9f4c4276b234addcdd2319b904217b21c2bf19bf
ObaidDev <abdellatif.hour.web@gmail.com>
2025-06-23 13:20:12 +0100

    Update Keycloak realm to 'task-swiftly'

--- front-end/src/keycloak.ts
+++ front-end/src/keycloak.ts
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
   url: 'http://localhost:8081', // Replace with your Keycloak server URL
-  realm: 'task-b2b', // Replace with your realm
+  realm: 'task-swiftly', // Replace with your realm
   clientId: 'hahn-task-management', // Replace with your clientId
 });
 
