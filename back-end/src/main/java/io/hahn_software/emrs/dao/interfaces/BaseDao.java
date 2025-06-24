13453c93296150f529a5d17658f10eb0d7557414
ObaidDev <abdellatif.hour.web@gmail.com>
2025-06-24 16:17:16 +0100

    Add BaseDao interface for generic data access operations

---
+++ back-end/src/main/java/io/hahn_software/emrs/dao/interfaces/BaseDao.java
@@ -0,0 +1,26 @@
+package io.hahn_software.emrs.dao.interfaces;
+
+
+import java.util.List;
+
+/***
+ * 
+ * 
+ * Base generic interface , that take entity and its ID (I =ID) , (T = type)
+ */
+public interface BaseDao <T, I>{
+
+    List<T> insertInBatch(List<T> entities);
+    
+    int deleteByIds(List<I> ids);
+    
+    List<T> findByIds(List<I> ids);
+    
+    List<T> findWithPagination(int page, int pageSize);
+    
+    Long count();
+
+    int updateInBatch(List<I> ids, T entity) ;
+
+    List<T> search(String keyword , int page, int pageSize) ;
+}
