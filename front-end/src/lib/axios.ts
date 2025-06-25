c3be420ef484687145499751a2a49b720934b1a9
ObaidDev <abdellatif.hour.web@gmail.com>
2025-06-25 17:54:39 +0100

    Add backApi instance and clean up request interceptor logging

--- front-end/src/lib/axios.ts
+++ front-end/src/lib/axios.ts
@@ -9,19 +9,24 @@ export const userApi = axios.create({
 });
 
 
+export const backApi = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_API_URL,
+});
+
+
 // Request interceptor for both instances
 const requestInterceptor = (config: any) => {
   const token = keycloak.token;
   // Get the token from localStorage or your auth state management
-  console.debug('token', `Bearer ${token}`) ;
-  
+    
   if (keycloak.authenticated) {
     config.headers.Authorization = "Bearer "  + token;
   }
   
-  console.debug('config', config);  
   return config;
 };
 
 
 userApi.interceptors.request.use(requestInterceptor);
+
+backApi.interceptors.request.use(requestInterceptor);
